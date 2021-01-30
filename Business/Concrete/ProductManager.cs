3031545531788f657abe26434a5ecb0da3c56ed6
Nihal Coşkun <nihalcoskkun@gmail.com>
2021-01-31 00:08:28 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        { //inmemory olabilir depper olabilir entityframework vs bunlardan biri gelecek
+          
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            //Yetkisi var mı ?
+
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Ientity implement eden class bir veritabanı tablosudur.
+    public interface IEntity
+    {
+
+    }
+}
