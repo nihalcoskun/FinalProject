0da3918411866e8a8763ba201f86d9da714d81d4
Nihal Coşkun <nihalcoskkun@gmail.com>
2021-02-24 00:47:18 +0300

    10.Gün

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -33,10 +33,22 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+
+                foreach (var product in productManager.GetProductDetails().Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+
         }
     }
 }
