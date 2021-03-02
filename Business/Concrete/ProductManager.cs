9b158eea42daaa3302d818607936aa79540a07f3
Nihal Coşkun <nihalcoskkun@gmail.com>
2021-03-03 00:37:27 +0300

    11.Gün

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -38,7 +38,7 @@ namespace Business.Concrete
         {
             //iş kodları
             //Yetkisi var mı ?
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 23)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
